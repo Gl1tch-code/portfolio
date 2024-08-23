eefa55030224d7e17758b9fc6a5082e99e070964
= <ibrahim.drissi@x-hub.io>
2024-08-23 18:00:53 +0100

    thanks page and contac me

--- main.js
+++ main.js
@@ -22,3 +22,40 @@ document
 document.querySelectorAll(".close").forEach((closer) => {
   closer.addEventListener("click", mobileMenu);
 });
+
+const form = document.getElementById("contact-me-form");
+
+emailjs.init({
+  publicKey: "8yDA8xqgWyprQp-74",
+});
+
+form.addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const formData = new FormData(form);
+  const formObject = {};
+
+  formData.forEach((value, key) => {
+    if (formObject[key]) {
+      if (!Array.isArray(formObject[key])) {
+        formObject[key] = [formObject[key]];
+      }
+      formObject[key].push(value);
+    } else {
+      formObject[key] = value;
+    }
+  });
+
+  document.querySelector('.form-submit-btn').style.display = "none";
+  document.querySelector('.form-loading-btn').style.display = "inline-block";
+
+  await emailjs
+    .send("service_glpg8gl", "template_c66aeqk", formObject)
+    .then((response) => {
+      if (response?.status === 200) {
+        window.location.pathname =
+          window.location.pathname === "/" || window.location.pathname === ""
+            ? "/thanks"
+            : window.location.pathname.replace("/index.html", "/thanks");
+      }
+    });
+});
